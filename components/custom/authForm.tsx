0e909dc0e3d4119af2348ac950e927096da32bf1
Shubham Kadariya <shubham.kadariya021@apexcollege.edu.np>
2025-07-05 10:29:21 +0545

    Fixing the Auth Pages

--- components/custom/authForm.tsx
+++ components/custom/authForm.tsx
@@ -14,6 +14,7 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./formField";
 import { useRouter } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa";
 
 //ShadCn Form Component:
 const authFormSchema = (type: any) => {
@@ -60,63 +61,104 @@ const AuthForm = ({ type }: any) => {
   const isSignup = type === "signup";
 
   return (
-    <div className="p-8 border-1 border-gray-200/25 rounded-2xl flex flex-col justify-center items-center w-[95%] lg:w-[30%] md:w-[55%] bg-black/25 lg:m-[2rem]">
-      <div className="w-full flex flex-col justify-start items-center mb-4 p-2  rounded-2xl bg-transparent">
-        <div className="w-full lg:w-[55%] flex justify-center items-center bg-transparent mb-2 lg:mb-0">
-          <div className="w-[34%] h-[18vh] lg:w-[40%] lg:h-[14vh] bg-transparent bg-motif mr-1"></div>
-          <h2 className="text-4xl font-semibold text-green-500/70">BolAi</h2>
-        </div>
-        <h3 className="text-md md:text-2xl lg:text-[1.05rem] text-green-400/90">
-          Speak. Practice. Succeed.
-        </h3>
-      </div>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full space-y-6"
+    <div className="w-full bg-black/25 flex flex-col justify-start items-center">
+      <div className="w-[95%] lg:w-[30%] md:w-[75%] mb-[2rem] lg:mb-[1.5rem] flex flex-col justify-evenly items-center">
+        {/* Back To Home Button */}
+        <Link
+          className="w-[55%] p-2 hover:cursor-pointer hover:text-green-400/90 flex justify-center items-center mt-[2rem] hover:underline"
+          href={"/"}
         >
-          {isSignup ? (
+          <FaArrowLeft className="text-md font-semibold text-gray-300/70 mr-6 hover:text-green-400/90" />
+          <div className="text-md sm:text-lg lg:text-md text-gray-300/70 hover:text-green-400/90">
+            Back to Home
+          </div>
+        </Link>
+        {/* Logo and Title */}
+        <div className="w-[55%] flex justify-center items-center p-2 mb-2">
+          {/* Logo */}
+          <div
+            className="w-[45%] h-[15vh] bg-transparent mr-2"
+            style={{
+              backgroundImage: `url('/motif.png')`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          {/* Title */}
+          <div className="text-4xl font-bold text-green-600">BolAi</div>
+        </div>
+        {/* Text and Description */}
+        <div className="w-[95%] lg:w-[75%] flex flex-col justify-around items-center">
+          <div className="text-2xl md:text-3xl lg:text-2xl font-bold text-white mb-2">
+            {isSignup ? "Create your account" : "Welcome back"}
+          </div>
+          <div className="text-sm md:text-lg lg:text-sm text-gray-200 text-center">
+            {isSignup
+              ? "Start your interview preparation journey today"
+              : "Sign in to continue your interview preparation"}
+          </div>
+        </div>
+      </div>
+      <div className="p-8 border-1 border-gray-200/25 rounded-2xl w-[95%] lg:w-[30%] md:w-[75%] bg-gray-900 flex flex-col justify-center items-center mb-6">
+        <div className="w-full flex flex-col justify-evenly items-center mb-[2rem]">
+          <div className="text-2xl md:text-3xl lg:text-2xl font-bold text-white mb-[1rem]">
+            {isSignup ? "Sign Up" : "Sign In"}
+          </div>
+          <div className="text-sm md:text-lg lg:text-sm text-white text-center">
+            {isSignup
+              ? "Create your account to get started with BolAi"
+              : "Enter your email and password to access your account"}
+          </div>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6"
+          >
+            {isSignup ? (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Your Name"
+              />
+            ) : (
+              <></>
+            )}
             <FormField
               control={form.control}
-              name="name"
-              label="Name"
-              placeholder="Your Name"
+              name="email"
+              label="Email"
+              type="email"
+              placeholder="Your Email Address"
             />
-          ) : (
-            <></>
-          )}
-          <FormField
-            control={form.control}
-            name="email"
-            label="Email"
-            type="email"
-            placeholder="Your Email Address"
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            label="Password"
-            type="password"
-            placeholder="Your Password"
-          />
-          <Button
-            type="submit"
-            className="hover:cursor-pointer bg-green-500/60 hover:bg-green-600 text-white transition-all ease-in-out duration-150 hover:scale-105 p-2 w-[85%] lg:w-[60%] h-[7.5vh]"
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="Your Password"
+            />
+            <Button
+              type="submit"
+              className="hover:cursor-pointer bg-green-500/60 hover:bg-green-600 text-white md:text-xl lg:text-sm transition-all ease-in-out duration-150 hover:scale-105 p-2 w-[85%] lg:w-[60%] h-[7.5vh] md:h-[6.5vh]"
+            >
+              {isSignup ? "Create an account" : "Access the account"}
+            </Button>
+          </form>
+        </Form>
+        <div className="m-[1.5rem] w-[85%] flex justify-center items-center">
+          <p className="text-center text-white text-sm md:text-lg lg:text-sm mr-2">
+            {isSignup ? "Have an account?" : "No account yet?"}
+          </p>
+          <Link
+            href={isSignup ? "/login" : "/signup"}
+            className="font-bold text-sm md:text-lg lg:text-sm text-blue-400 underline transition-all ease-in-out duration-150 hover:scale-110"
           >
-            {isSignup ? "Create an account" : "Access the account"}
-          </Button>
-        </form>
-      </Form>
-      <div className="m-[1.5rem] w-[85%] flex justify-center items-center">
-        <p className="text-center text-white text-sm lg:text-md mr-2">
-          {isSignup ? "Have an account?" : "No account yet?"}
-        </p>
-        <Link
-          href={isSignup ? "/login" : "/signup"}
-          className="font-bold text-sm lg:text-md text-blue-400 underline transition-all ease-in-out duration-150 hover:scale-110"
-        >
-          {isSignup ? "Sign In" : "Sign Up"}
-        </Link>
+            {isSignup ? "Sign In" : "Sign Up"}
+          </Link>
+        </div>
       </div>
     </div>
   );
--- components/custom/formField.tsx
+++ components/custom/formField.tsx
@@ -22,13 +22,13 @@ const FormField = ({
     control={control}
     render={({ field }) => (
       <FormItem>
-        <FormLabel>{label}</FormLabel>
+        <FormLabel className="md:text-lg lg:text-sm">{label}</FormLabel>
         <FormControl>
           <Input
             placeholder={placeholder}
             {...field}
             type={type}
-            className="h-[7.5vh] lg:h-[6.5vh] px-4 focus:outline-none focus:ring-2 focus:ring-green-500 !ring-green-400/90 my-2"
+            className="h-[7.5vh] md:h-[6.75vh] lg:h-[6.5vh] px-4 focus:outline-none focus:ring-2 focus:ring-green-500 !ring-green-400/90 my-2 placeholder:text-base md:placeholder:text-lg lg:placeholder:text-sm"
           />
         </FormControl>
         <FormMessage />
